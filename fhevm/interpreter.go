4e23194f958a0924399badbd7306b3130325c755
youben11 <ayoub.benaissa@zama.ai>
2023-11-10 11:02:42 +0100

    feat: add integration function to eval remaining OptReqs
    This functions is meant to be used as part of vm.EVMInterpreter.Run
    function when there is an errStopToken

--- fhevm/interpreter.go
+++ fhevm/interpreter.go
@@ -1,6 +1,8 @@
 package fhevm
 
-import "github.com/ethereum/go-ethereum/common"
+import (
+	"github.com/ethereum/go-ethereum/common"
+)
 
 type ScopeContext interface {
 	GetMemory() Memory
@@ -59,4 +61,23 @@ type PrivilegedMemory struct {
 var PrivilegedMempory *PrivilegedMemory = &PrivilegedMemory{
 	make(map[common.Hash]*verifiedCiphertext),
 	make([]*tfheCiphertext, 0),
+}
+
+// Evaluate remaining optimistic requires when Interpreter.Run get an errStopToken
+//
+// This function is meant to be integrated as part of vm.EVMInterpreter.Run in the case
+// there was an errStopToken
+func EvalRemOptReqWhenStopToken(env EVMEnvironment) (err error) {
+	err = nil
+	// If we are finishing execution (about to go to from depth 1 to depth 0), evaluate
+	// any remaining optimistic requires.
+	if env.GetDepth() == 1 {
+		result, evalErr := evaluateRemainingOptimisticRequires(env)
+		if evalErr != nil {
+			err = evalErr
+		} else if !result {
+			err = ErrExecutionReverted
+		}
+	}
+	return err
 }
