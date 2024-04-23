4f8b5aea2bd6660c323e508514b68214576cbd1b
LoveNoble <154249866+lovenoble@users.noreply.github.com>
2024-04-23 19:46:01 +0900

    feat : add cks import part (#12)
    * feat: add cks import part
    
    * add comment about cks

--- fhevm/tfhe/tfhe_key_management.go
+++ fhevm/tfhe/tfhe_key_management.go
@@ -97,11 +97,20 @@ func InitGlobalKeysFromFiles(keysDir string) error {
 	if err != nil {
 		return err
 	}
+	// TODO: This is a temporary measure until we remove sync decrypt for FHE operations
+	// ref: https://github.com/Inco-fhevm/fhevm-go/issues/13
+	var cksPath = path.Join(keysDir, "cks")
+	cksBytes, err := os.ReadFile(cksPath)
+	if err != nil {
+		return err
+	}
 
 	sks = C.deserialize_server_key(toDynamicBufferView(sksBytes))
 
 	pksHash = crypto.Keccak256Hash(pksBytes)
 	pks = C.deserialize_compact_public_key(toDynamicBufferView(pksBytes))
+
+	cks = C.deserialize_client_key(toDynamicBufferView(cksBytes))
 
 	initCiphertextSizes()
 
